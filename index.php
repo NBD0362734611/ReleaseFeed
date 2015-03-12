c3a3d34b46ea4c7463678a6cdc0c948e3b59a08c
tatsumi <dskt0118@gmail.com>
2015-03-12 12:10:13 +0000

    201503012 tatsumi

--- index.php
+++
@@ -1,9 +0,0 @@
-
-<a href='index.html'>トップLP</a><br>
-<a href='newrelease.html'>新着リリース</a><br>
-<a href='release-post.html?rid=813531'>リリース詳細</a><br>
-<a href='mypage-pocket.html'>新聞1</a><br>
-<a href='paper2.html'>新聞2</a><br>
-<a href='contact.html'>コンタクト</a><br>
-<a href='timeline.html'>タイムライン</a><br>
-
